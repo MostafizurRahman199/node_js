fb12dda85911d11990da14d4e5e3efcb2ca860b2
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-06-04 16:06:35 +0600

    create server and do routing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+
 // console.log(fs);
 
 const hello = "Hello world";
@@ -12,16 +13,57 @@ const hello = "Hello world";
 
 //blocking asynchronous way
 
-fs.readFile("./txt/file1.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    fs.readFile(`./txt/${data2}.txt`, "utf-8", (err, data3) => {
-      fs.readFile(`./txt/${data3}.txt`, "utf-8", (err, data4) => {
-        fs.writeFile(`./txt/input.txt`, `${data4}`, `utf-8`, (err) => {
-          console.log(`File has been written👍`);
-        });
-      });
-    });
-  });
-});
+// fs.readFile("./txt/file1.txt", "utf-8", (err, data1) => {
+    //   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        //     fs.readFile(`./txt/${data2}.txt`, "utf-8", (err, data3) => {
+            //       fs.readFile(`./txt/${data3}.txt`, "utf-8", (err, data4) => {
+                //         fs.writeFile(`./txt/input.txt`, `${data4}`, `utf-8`, (err) => {
+                    //           console.log(`File has been written👍`);
+                    //         });
+                    //       });
+                    //     });
+                    //   });
+                    // });
+                    
+                    // console.log("Read file");
+                    
+                    
+                    
+//server
 
-console.log("Read file");
+const http = require('http');
+const url = require('url');
+
+
+const server = http.createServer((req, res)=>{
+   const pathname = req.url;
+
+   console.log(pathname);
+
+   if(pathname === '/login' || pathname=== '/'){
+    res.end("You want to login")
+   }
+   else if(pathname === '/signup'){
+    res.end("You want to signup");
+   }
+   else{
+    res.writeHead(404,{
+        'Content-type':'text/html',
+    });
+    res.end(`<h1>Page not found</h1>`);
+   }
+
+
+ 
+})
+
+
+server.listen('8000', '127.0.0.1', ()=>{
+    console.log('Hello this server is listening on port 8000');
+})
+
+
+
+
+
+
