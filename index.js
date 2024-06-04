d3fe98eccd1049505b1325d2684146811127006d
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-06-04 14:34:09 +0600

    create call back hell

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-
-const fs = require('fs');
+const fs = require("fs");
 // console.log(fs);
 
 const hello = "Hello world";
 // console.log(hello);
 
+// //blocking synchrous way
+// const textInput = fs.readFileSync("./txt/input.txt", 'utf-8' )
+// console.log(textInput);
+// const textOut = `${textInput}\n We also love Barishal , It's my home town.\n ${Date.now()}`;
+// fs.writeFileSync('./txt/input.txt', textOut);
 
-const textInput = fs.readFileSync("./txt/input.txt", 'utf-8' )
-console.log(textInput);
+//blocking asynchronous way
 
-const textOut = `${textInput}\n We also love Barishal , It's my home town.\n ${Date.now()}`;
-fs.writeFileSync('./txt/input.txt', textOut);
+fs.readFile("./txt/file1.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.readFile(`./txt/${data2}.txt`, "utf-8", (err, data3) => {
+      fs.readFile(`./txt/${data3}.txt`, "utf-8", (err, data4) => {
+        fs.writeFile(`./txt/input.txt`, `${data4}`, `utf-8`, (err) => {
+          console.log(`File has been written👍`);
+        });
+      });
+    });
+  });
+});
 
+console.log("Read file");
